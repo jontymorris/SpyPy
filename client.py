d8e2fc58bd74f322e17f81cd6fac14ebff813847
Jonty Morris <jonty.morris@yahoo.co.nz>
2018-08-22 08:16:45 +1200

    Improved the disconnecting

--- client.py
+++ client.py
@@ -9,6 +9,8 @@ class ControllerHandler:
     ''' This acts as a gateway for interacting with the controller '''
 
     def __init__(self, address):
+        self.delimiter = "\n\n".encode("utf-8")
+
         self.event_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.event_socket.bind((address, 5790))
 
@@ -21,50 +23,64 @@ class ControllerHandler:
         self.image_handler()
     
     def image_handler(self):
+        ''' When a new connection is made, screenshots will be taken, pickled up and sent over through the socket '''
         self.image_socket.listen(1)
-        delimiter = "\n\n".encode("utf-8")
 
         while True:
             conn, address = self.image_socket.accept()
-            
             try:
                 while True:
                     screenshot = get_screenshot()
                     data = pickle.dumps(screenshot)
                     
                     conn.send(data)
-                    conn.send(delimiter)
+                    conn.send(self.delimiter)
                     
                     sleep(0.01)
-            except Exception as exp:
-                print(exp)
-            
-            conn.close()
+            except:
+                conn.close()
 
     def event_handler(self):
+        ''' Listens for incoming events from the controller. When an event is received, it is then dispatched to the event method.'''
         self.event_socket.listen(1)
-
+        
         while True:
             print("Now listening...")
+
             conn, address = self.event_socket.accept()
-            print("Connection from " + address[0] + "\n")
+            print("\nConnection from " + address[0])
 
             try:
+                event_data = []
                 while True:
-                    data = conn.recv(100)
+                    data = conn.recv(256)
+                    
+                    if data.endswith(self.delimiter):
+                        data = data[:-2]
 
-                    if data:
-                        options = json.loads(data.decode("utf-8"))
+                        if data != []:
+                            event_data.append(data)
 
-                        if options["action"] == "click":
-                            click(options["x"], options["y"], options["mouse_button"])
+                        try:
+                            event_bytes = b''.join(event_data)
+                            options = json.loads(event_bytes.decode("utf-8"))
+
+                            if options["action"] == "disconnect":
+                                break
+                            else:
+                                event(options)
+                            
+                        except Exception as exp:
+                            print(exp)
                         
-                        elif options["action"] == "key":
-                            press_key(options["char"])
-            except Exception as exp:
-                print(exp)
-            
-            conn.close()
+                        event_data = []
+                    else:
+                        event_data.append(data)
+            except:
+                pass
+            finally:
+                print("Disconnected\n")
+                conn.close()
 
 
 def get_screenshot():
@@ -73,6 +89,14 @@ def get_screenshot():
     image_data = image_data.resize((int(image_data.width/1.5), int(image_data.height/1.5)), Image.ANTIALIAS)
 
     return image_data
+
+def event(options):
+    ''' Triggers an event to happen on the system '''
+    if options["action"] == "click":
+        click(options["x"], options["y"], options["mouse_button"])
+
+    elif options["action"] == "key":
+        press_key(options["char"])
 
 def click(x, y, mouse_button):
     ''' Scales up the cordinates and clicks a mouse button at the new location ''' 
--- controller.py
+++ controller.py
@@ -8,54 +8,70 @@ class ClientHandler:
     ''' This acts as a gateway for interacting with the client '''
 
     def __init__(self, address):
+        self.delimiter = "\n\n".encode("utf-8")
+
         self.event_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.event_socket.connect((address, 5790))
-        
+
+        self.is_connected = True
+
         self.image_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.image_socket.connect((address, 5791))
 
-        self.is_running = True
         self.image_thread = Thread(target=self.image_handler)
         self.image_thread.start()
 
         print("Connected to " + address)
     
     def disconnect(self):
-        self.is_running = False
+        ''' Disconnects from the client '''
+        self.is_connected = False
+
+        options = {"action": "disconnect"}
+        self.event_socket.sendall(json.dumps(options).encode("utf-8"))
+        self.event_socket.sendall(self.delimiter)
+
         self.event_socket.close()
+        self.image_socket.close()
+        
+        print("Disconnected from client")
 
     def image_handler(self):
+        ''' Receives images through a socket from the client. It then unpickles the images and displays them on to the window '''
         image_data = []
-        delimiter = "\n\n".encode("utf-8")
-
-        while self.is_running:
-            data = self.image_socket.recv(4096)
-            
-            if data.endswith(delimiter):
-                data = data[:-2]
-
-                if data != []:
-                    image_data.append(data)
-
-                try:
-                    pickled_image = b''.join(image_data)
-                    screenshot = pickle.loads(pickled_image)
-
-                    tk_image = ImageTk.PhotoImage(screenshot)
-                    
-                    image_label.configure(image=tk_image)
-                    image_label.image = tk_image
-                except Exception as exp:
-                    print(exp)
+        while self.is_connected:
+            try:
+                data = self.image_socket.recv(4096)
                 
-                image_data = []
+                if data.endswith(self.delimiter):
+                    data = data[:-2]
 
-            else:
-                image_data.append(data)
-        
-        self.image_socket.close()
+                    if data != []:
+                        image_data.append(data)
+
+                    try:
+                        pickled_image = b''.join(image_data)
+                        screenshot = pickle.loads(pickled_image)
+
+                        tk_image = ImageTk.PhotoImage(screenshot)
+                        
+                        image_label.configure(image=tk_image)
+                        image_label.image = tk_image
+                    except:
+                        pass
+                    
+                    image_data = []
+
+                else:
+                    image_data.append(data)
+            except Exception as exp:
+                print(exp)
 
     def send_click(self, x, y, mouse_button):
+        ''' Sends a mouse event to the client '''
+        if not self.is_connected:
+            return
+
         options = {
             "action": "click",
             "x": x,
@@ -64,43 +80,63 @@ class ClientHandler:
             }
         
         data = json.dumps(options).encode("utf-8")
+
         self.event_socket.sendall(data)
+        self.event_socket.sendall(self.delimiter)
     
     def send_key(self, key):
+        ''' Sends key press event to the client'''
+        if not self.is_connected:
+            return
+
         options = {
             "action": "key",
             "char": key
             }
 
         data = json.dumps(options).encode("utf-8")
+
         self.event_socket.sendall(data)
+        self.event_socket.sendall(self.delimiter)
 
 
 def left_click(event: Event):
+    ''' Sends a left click event to the client '''
     client.send_click(event.x, event.y, "left")
 
 def right_click(event: Event):
+    ''' Sends a right click event to the client '''
     client.send_click(event.x, event.y, "right")
 
 def middle_click(event: Event):
+    ''' Sends a midle click event to the client '''
     client.send_click(event.x, event.y, "middle")
 
 def key_callback(event: Event):
+    ''' Sends the pressed key to the client '''
     client.send_key(event.char)
 
 
+# Creates the window
 root = Tk()
 root.title("PySpy")
 root.resizable(False, False)
 
+# Displays the clients screenshots
 image_label = Label(root)
 image_label.pack()
 
+# Hooks for the tkinter window
 root.bind("<Key>", key_callback)
 image_label.bind("<Button-1>", left_click)
 image_label.bind("<Button-2>", middle_click)
 image_label.bind("<Button-3>", right_click)
 
+# Connect to the client
 client = ClientHandler("192.168.1.194")
+
+# Start displaying the window
 root.mainloop()
+
+# Disconnect when the window has been closed
 client.disconnect()
